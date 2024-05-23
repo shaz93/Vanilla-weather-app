3fdc96fa65218d3828f053d99c9c7f797f62ebca
shaz93 <sheronndimande93@gmail.com>
2024-05-23 16:24:27 +0200

    updated more weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    let description = document.querySelector("#description");
+    let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time")
-    let date = newDate(response.data.time *1000);
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
 
 
@@ -14,7 +14,7 @@ cityElement.innerHTML = response.data.city;
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-timeElement.innerHTML = formateDate(date)
+timeElement.innerHTML = formatDate(date);
 temperatureElement.innerHTML = Math.round(temperature);
 iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}"class="weather-app-icon"/>`;
 }
@@ -22,13 +22,22 @@ iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}"class="
 function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
     let day = days[date.getDay()];
 
     if (minutes < 10) {
         minutes = `0 ${minutes}`;
     }
-    return `$ {day} $ {hours}:$ {minutes}`;
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -44,6 +53,7 @@ function handleSearchSubmit (event) {
     let searchInput = document.querySelector("#search-form-input");
    let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
